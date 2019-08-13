3113bd3569322fd058cc7915fb22280a0c8af1f1
MasonTwoK <MasonTwoK@users.noreply.github.com>
2019-08-13 08:43:23 +0300

    Added exercise 9 with comments and own rewrite

---
+++ ex9.py
@@ -0,0 +1,30 @@
+#ex9.py Printing, Printing, Printing
+#Here's some new strange stuff, remember type it exactly.
+'''
+days = "Mon Tue Wen Thu Fri Sat Sun" #Casual variable which contains string
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #variable contains string with carret return symbol \n 
+# function print returns string  and variable 
+print("Here are the days:" + days) 
+print("Here are the months:", months)
+# function prints 4 lines with """ """
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as musch as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+'''
+#Rewrite of excircise 9
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("ReWRITE Executes")
+print("Here are the days:", days)
+print("Here are the months: ", months)
+
+print("""
+There's somthing going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
