497a18cdbdcb27a4c86d1f8460c84fe8883379a1
MasonTwoK <35229951+MasonTwoK@users.noreply.github.com>
2019-08-16 19:27:58 +0300

    Exercise 12 completed with rewrite

---
+++ ex12.py
@@ -0,0 +1,13 @@
+#Exercise 12
+'''
+age = input("How old are you?" )
+height = input("How tall are you?" )
+weight = input("How much do you weigh?" )
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+'''
+#Rewrite
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+print(f"So you are {age} years old, your height is {height} and your weight is {weight}")
