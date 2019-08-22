a24b8e202c86341b399afda38cc6fd1bc0fd62d8
MasonTwoK <35229951+MasonTwoK@users.noreply.github.com>
2019-08-22 19:20:00 +0300

    Added exercise 15 (script file and samples files)

---
+++ ex15/ex15.py
@@ -0,0 +1,30 @@
+'''
+from sys import argv #adds module argv
+
+script, filename = argv #send arguments to variables
+
+txt = open(filename) # returns file object
+
+print(f"Here's your file {filename}:") # writes string in terminal
+print(txt.read()) # .read returns 
+txt.close()
+
+print("Type the filename again:")
+file_again = input("Type the filename again:\n> ")
+
+txt_again = open(file_again) #this line returns file object
+
+print(txt_again.read()) # .read() this method returns whole text
+txt_again.close()
+'''
+#Rewrite of exercise 
+from sys import argv
+
+script, file_name = argv
+
+txt = open(file_name)
+print(f"File {file_name} contains:\n" + txt.read())
+
+ins_file = input("Enter name of file for open it:\n> ")
+int_txt = open(ins_file)
+print(int_txt.read())
