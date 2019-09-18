9632fd6905fb11c404b2319fb8ff4e944cb0e5ed
MasonTwoK <35229951+MasonTwoK@users.noreply.github.com>
2019-09-18 20:43:35 +0300

    Added exercises 27 and 28.

---
+++ ex27.py
@@ -0,0 +1,53 @@
+# Exersice 27 
+# Memorizing Logic
+
+'''
+*OPERATORS*
+
+and      
+or
+not 
+!= (not equal)
+== (equal)
+>= (greater-than-equal)
+<= (less-than-equal)
+True
+False
+'''
+
+'''
+*TASK*
+
+not False => True
+not True  => False
+
+True or False  => True
+True or True   => True
+False or True  => True
+False or False => False
+
+True and False  => False
+True and True   => True
+False and True  => False
+False and False => False
+
+not (True or False)  => False
+not (True or True)   => False
+not (False or True)  => False
+not (False or False) => True
+
+not (True and False)  => True
+not (True and True)   => False
+not (False and True)  => True
+not (False and False) => True
+
+1 != 0 => True
+1 != 1 => False
+0 != 1 => True
+0 != 0 => False
+
+1 == 0 => False
+1 == 1 => True
+0 == 1 => False
+0 == 0 => True
+'''
