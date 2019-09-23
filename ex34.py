89f67f239ad41ca44ec038ba46c71802d5b3ff04
MasonTwoK <35229951+MasonTwoK@users.noreply.github.com>
2019-09-23 19:25:25 +0300

    Added exercise 34 with completed drills

---
+++ ex34.py
@@ -0,0 +1,25 @@
+# Accessing Elements of Lists
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals_drill = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"Here is animals_drill list: {animals_drill}")
+
+# 1. The animal at 1.
+print(f"The animal at 1 is {animals_drill[1]}")
+# 2. The third (3rd) animal.
+print(f"The third (3rd) animal is {animals_drill[2]}") 
+# 3. The first (1st) animal.
+print(f"The first (1st) animal is {animals_drill[0]}")
+# 4. The animal at 3.
+print(f"The animal at 3 is {animals_drill[3]}")
+# 5. The fifth (5th) animal.
+print(f"The fifth (5th) animal is {animals_drill[4]}")
+# 6. The animal at 2.
+print(f"The animal at 2 is {animals_drill[2]}")
+# 7. The sixth (6th) animal.
+print(f"The sixth (6th) animal is {animals_drill[5]}")
+# 8. The animal at 4.
+print(f"The animal at 4 is {animals_drill[4]}")
